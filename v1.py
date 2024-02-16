ac78ab2e8d4a6d376dafe22a650ff6a6f9cc4804
SarthakBhardwajCode <85218961+sarthakbhardwaj27@users.noreply.github.com>
2024-02-16 23:46:47 +0530

    uncommented v1

--- v1.py
+++ v1.py
@@ -1,22 +1,22 @@
-# import numpy as np 
-# import cv2 as cv
+import numpy as np 
+import cv2 as cv
 
-# cap = cv.VideoCapture(0)
-# if not cap.isOpened():
-#     print("Cannot open camera")
-#     exit
-# while True:
-#     ret, frame = cap.read()
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit
+while True:
+    ret, frame = cap.read()
 
-#     if not ret:
-#         print("Can't receive frame (stream end?). Existing ...")
-#         break
-#     # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-#     cv.imshow('Hand Tracking',frame)
-#     if cv.waitKey(1) == ord('q'):
-#         break
-# cap.release()
-# cv.destroyAllWindows()
+    if not ret:
+        print("Can't receive frame (stream end?). Existing ...")
+        break
+    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('Hand Tracking',frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
 
 
 
